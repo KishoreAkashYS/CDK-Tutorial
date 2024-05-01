ab03bdef0659acf9079c34b76aaf8c8ada51d256
KishoreAkashYS <kishore12akash@gmail.com>
2024-05-01 17:35:44 +0530

    Initial commit

--- lib/ts_resource-stack.ts
+++ lib/ts_resource-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TsResourceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Create an L2 construct for S3 Bucket that deletes on stack destroy
+    const bucket1 = new Bucket(this, "KishoreBuck123", {
+          versioned: true, 
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsResourceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
+
